0a1e6c9568d9a2f4b6032d52dbd318d899347c11
emmadonjo <jo4christ48@yahoo.com>
2026-01-30 07:19:05 +0100

    last

--- backend/prod.sh
+++ backend/prod.sh
@@ -3,8 +3,5 @@ set -e
 mkdir -p /var/www/html/storage/logs /var/www/html/bootstrap/cache
 chmod -R ug+rwX /var/www/html/storage /var/www/html/bootstrap/cache
 
-
-php artisan migrate --seed --force
-
 # start main process
 exec "$@"
