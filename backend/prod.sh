fe0daa1761923465501d575f320a39cda40cac65
emmadonjo <jo4christ48@yahoo.com>
2026-01-30 06:54:55 +0100

    removed duplicate db health check

--- backend/prod.sh
+++ backend/prod.sh
@@ -4,11 +4,6 @@ set -e
 find /var/www/html/storage /var/www/html/bootstrap/cache -type d -exec chmod 775 {} +
 find /var/www/html/storage /var/www/html/bootstrap/cache -type f -exec chmod 664 {} +
 
-echo "Waiting for database..."
-until mysqladmin ping -h"$DB_HOST" -u"$DB_USERNAME" -p"$DB_PASSWORD" --silent; do
-  sleep 2
-done
-
 php artisan migrate --seed --force
 
 # Start main process
